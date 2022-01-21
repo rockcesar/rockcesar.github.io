2bb2d131baa17c56e8fea7796189a6b551e5349c
ccordero <cesar.cordero.r@gmail.com>
2022-01-21 09:38:10 -0400

    Update!

---
+++ chess/js/board-time.js
@@ -0,0 +1,99 @@
+function startTimer(game) {
+
+  $('#game-timer').removeClass('hidden');
+
+  var timeSec = 0, timeMin = 0;
+  var timeSecStr = '', timeMinStr = '';
+
+  clearInterval(gameTimer);
+
+  gameTimer = setInterval(function() { 
+
+    timeSec++;
+    if (timeSec >= 0 && timeSec <= 9) {
+      timeSecStr = '0' + timeSec;
+    }
+    if (timeSec >= 10 && timeSec <= 59) {
+      timeSecStr = timeSec;
+    }
+    if (timeSec == 60) {
+      timeSecStr = '00';
+      timeSec = 0;
+      timeMin++;
+    }
+
+    if (timeMin >= 0 && timeMin <= 9) {
+      timeMinStr = '0' + timeMin;
+    }
+    if (timeMin >= 10 && timeMin <= 59) {
+      timeMinStr = timeMin;
+    }
+
+    $('#game-timer').text(timeMinStr + ':' + timeSecStr);
+
+    if (timeMin >= 5) {
+      clearInterval(gameTimer);
+    }
+
+  }, 1000);
+
+    if(game == undefined || game == "stop-both")
+    {
+        $('.timer_white').countimer('start');
+        $('.timer_white').countimer('stop');
+        $('.timer_black').countimer('start');
+        $('.timer_black').countimer('stop');
+    }else if(game.turn() == "b")
+    {
+    	$('.timer_white').countimer('stop');
+    	$('.timer_black').countimer('resume');
+    }else if(game.turn() == "w"){
+	$('.timer_black').countimer('stop');
+        $('.timer_white').countimer('resume');
+    }
+
+    if(game == undefined || game == "stop-both")
+    {
+    }else if (game.in_checkmate()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }else if (game.in_draw()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }else if (game.in_stalemate()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }
+}
+
+function stopTimer(game) {
+  clearInterval(gameTimer);
+  $('#game-timer').text('00:00');
+
+  if(game == undefined || game == "stop-both")
+    {
+        $('.timer_white').countimer('start');
+        $('.timer_white').countimer('stop');
+        $('.timer_black').countimer('start');
+        $('.timer_black').countimer('stop');
+    }else if(game.turn() == "b")
+    {
+        $('.timer_white').countimer('stop');
+        $('.timer_black').countimer('resume');
+    }else if(game.turn() == "w"){
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('resume');
+    }
+
+    if(game == undefined || game == "stop-both")
+    {}else if (game.in_checkmate()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }else if (game.in_draw()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }else if (game.in_stalemate()) {
+        $('.timer_black').countimer('stop');
+        $('.timer_white').countimer('stop');
+    }
+}
